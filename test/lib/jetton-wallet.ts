0b39a15217ab22541562eface697287eecd7b45e
Shahar Yakir <shahar.yakir@gmail.com>
2022-05-29 12:08:37 +0300

    wallet transfer test

---
+++ test/lib/jetton-wallet.ts
@@ -0,0 +1,23 @@
+// TODO: possibly use this outside tests
+
+import BN from "bn.js";
+import { Address, beginCell, Cell, Slice, toNano } from "ton";
+import { WrappedSmartContract } from "./contract-deployer";
+import { OPS } from "./ops";
+
+export class JettonWallet extends WrappedSmartContract {
+
+    static transferBody(toOwnerAddress: Address, jettonValue: BN): Cell {
+        return beginCell()
+            .storeUint(OPS.Transfer, 32)
+            .storeUint(0, 64) // queryid
+            .storeCoins(jettonValue)
+            .storeAddress(toOwnerAddress)
+            .storeAddress(null) // TODO RESP?
+            .storeDict(null) // custom payload
+            .storeCoins(0) // forward ton amount TODO
+            .storeRefMaybe(null) // forward payload - TODO??
+            .endCell()
+    }
+
+}
