a0813c074ac74ba1f4a9e87091a8bca74194cfa1
Shahar Yakir <shahar.yakir@gmail.com>
2022-06-08 13:06:45 +0300

    remove dist from git

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./lib/deploy-controller";
-export * from "./lib/env-profiles";
-export * from "./lib/contract-deployer";
-export * from "./lib/transaction-sender";
--- dist/lib/consts.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const WORKCHAIN = 0;
