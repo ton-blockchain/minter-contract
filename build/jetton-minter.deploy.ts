4827c242ee5adb7a75a30b0ecbb373a9d97e032b
Shahar Yakir <shahar.yakir@gmail.com>
2022-06-30 16:40:51 +0300

    -

--- build/jetton-minter.deploy.ts
+++ build/jetton-minter.deploy.ts
@@ -50,7 +50,7 @@ export function buildTokenMetadataCell(data: { [s: string]: string | undefined }
 
     let bufferToStore = Buffer.from(v, jettonOnChainMetadataSpec[k as JettonMetaDataKeys]);
 
-    const CELL_MAX_SIZE_BYTES = 750 / 8; // TODO figure out this number
+    const CELL_MAX_SIZE_BYTES = Math.floor(1023 / 8);
 
     const rootCell = new Cell();
     let currentCell = rootCell;
